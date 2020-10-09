d70c0bd1385226659128672750b198a3dfdd27c1
Alex <alex.andrul10@gmail.com>
2020-10-09 20:50:58 +0100

    start

---
+++ frontend/src/Components/Home.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Carousel from "react-bootstrap/Carousel";
+
+import room from "../assets/room.jpg";
+import room2 from "../assets/room2.jpg";
+import room3 from "../assets/room3.jpg";
+
+const Home = () => {
+  return (
+    <Carousel>
+      <Carousel.Item>
+        <img className="d-block w-100" src={room2} alt="First slide" />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img className="d-block w-100" src={room} alt="Second slide" />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img className="d-block w-100" src={room3} alt="Third slide" />
+      </Carousel.Item>
+    </Carousel>
+  );
+};
+
+export default Home;
